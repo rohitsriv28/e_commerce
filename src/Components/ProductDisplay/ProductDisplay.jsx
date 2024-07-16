8bd4e534ffc0818c127736e50da2ec3a74fe9180
rohitsriv28 <rohitraj2002ind@gmail.com>
2024-07-16 12:08:30 +0545

    Desktop Frontend Part Almost Done

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from "../Assests/star_icon.png";
 import star_dull_icon from "../Assests/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
+  const { addToCart } = useContext(ShopContext);
   return (
     <div className="productDisplay">
       <div className="productDisplay-left">
@@ -53,7 +55,8 @@ const ProductDisplay = (props) => {
             <div>XXL</div>
           </div>
         </div>
-        <button>ADD TO CART</button>
+        <button onClick={() => addToCart(product.id)}>ADD TO CART</button>
+
         <p className="productDisplay-right-category">
           <span>Category: </span>Women, T-Shirt, Crop Top
         </p>
