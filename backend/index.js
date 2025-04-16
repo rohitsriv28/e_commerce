a11425fc1c52565053c2928fc4cda1aad1c3f162
rohitsriv28 <rohitraj2002ind@gmail.com>
2025-04-16 14:14:03 +0530

    All functionalities working nicely, the project is complete.

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,7 @@ app.use(cors());
 
 //Database Connectio with MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("MongoDB connection error:", error));
 
